136f53c1971a49610262855af8ad994c68785658
xiaoge56 <1493578498@qq.com>
2015-07-22 19:54:43 +0800

    upda

--- Project3.py
+++ Project3.py
@@ -26,7 +26,7 @@ class Cluster:
         """
         String representation assuming the module is "alg_cluster".
         """
-        rep = "alg_cluster.Cluster("
+        rep = "Cluster("
         rep += str(self._fips_codes) + ", "
         rep += str(self._horiz_center) + ", "
         rep += str(self._vert_center) + ", "
@@ -132,14 +132,6 @@ class Cluster:
             total_error += (singleton_distance ** 2) * singleton_cluster.total_population()
         return total_error
             
-        
-            
-
-        
-    
-    
-            
-
 
 def slow_closest_pair(cluster_list):
     'find closest pair in cluster_list'
@@ -251,26 +243,75 @@ def hierarchical_clustering(cluster_list, num_clusters):
         cluster_list.remove(pomit2)
         k_cluster=len(cluster_list)
     return cluster_list
-def kmeans_clustering(cluster_list, num_clusters, num_iterations):
+def kmeans_clustering1(cluster_list, num_clusters, num_iterations):
     'find K clusters in some points or data by hierarchical_clustering'
-    total_number=len(cluster_list)
-    init_centroids=cluster_list[:num_clusters]
-    for n_time in range(num_iterations):
+
+    newss= [c.copy() for c in cluster_list]
+    centroids=newss[:num_clusters]
+    ret_cls=[]
+    for n_time in range(num_iterations):#点循环
         emtpys=[]
-        for m in range(num_clusters):
+        for m_num in range(num_clusters):
             emtpys.append([])
         for point in cluster_list:
 
-                dist=map(lambda x:x.distance(point),init_centroids)
-                dist.remove(0.0)
-                dist.remove(0)
+                dist=map(lambda x:x.distance(point),centroids)
+                # dist.pop(dist.index(min(dist)))
+
+                
+                # dist.remove(0)
                 ind=dist.index(min(dist))#第几个centroids最近
-                emtpys[ind].extend([point,init_centroids[ind]])
-        #updata cent
-        newcent=list(init_centroids)
+                emtpys[ind].append(point)
         for cent_index in range(num_clusters):
-            mass=set(init_centroids[cent_index])
-            point=reduce(lambda point1,ponit2:point1.merge_clusters(ponit2),mass)
-            newcent[cent_index]=point
-        init_centroids=list(newcent)
-    return
+            # mass=emtpys[cent_index]
+            mass=[x.copy() for x in emtpys[cent_index]]
+
+            if len(mass)>=2:
+                point=reduce(lambda point1,ponit2:point1.merge_clusters(ponit2),mass)
+                # for removepoint in mass[1:]:
+                #     emtpys[cent_index].remove(removepoint)
+            else:
+                point=mass[0]
+            centroids[cent_index]=point
+        # init_centroids=list(newcent)
+        ret_cls=emtpys
+    cluster_list.pop()
+    return ret_cls
+
+
+def kmeans_clustering(cluster_list, num_clusters, num_iterations):
+    'find K clusters in some points or data by hierarchical_clustering'
+    total_n=len(cluster_list)
+    new_cluster_list=[c.copy() for c in cluster_list]
+    centroids=new_cluster_list[:total_n]
+
+    fanhui=[]
+    while num_iterations>0:
+        num_iterations-=1
+        retset=[]
+        for m_num in range(num_clusters):
+            retset.append([])
+        for point in new_cluster_list:
+            dist=[]
+            for centroid in centroids:
+                print point,centroid,dist
+                dist.append(point.distance(centroid))
+            index=dist.index(min(dist))
+            retset[index].append(point)
+        for index in range(num_clusters):
+            for point in retset:
+                if len(point)==1:
+                    new_cent=point
+                else:
+                    massset=[c.copy() for c in point]
+                    new_cent=reduce(lambda x,y:x.merge_clusters(y),massset)
+
+            centroids[index]=new_cent
+        fanhui=retset
+    return fanhui
+m= kmeans_clustering([Cluster(set(['01073']), 704.191210749, 411.014665198, 662047, 7.3e-05), Cluster(set(['06059']), 113.997715586, 368.503452566, 2846289, 9.8e-05), Cluster(set(['06037']), 105.369854549, 359.050126004, 9519338, 0.00011), Cluster(set(['06029']), 103.787886113, 326.006585349, 661645, 9.7e-05), Cluster(set(['06071']), 148.402461892, 350.061039619, 1709434, 7.7e-05), Cluster(set(['06075']), 52.7404001225, 254.517429395, 776733, 8.4e-05), Cluster(set(['08031']), 371.038986573, 266.847932979, 554636, 7.9e-05), Cluster(set(['24510']), 872.946822486, 249.834427518, 651154, 7.4e-05), Cluster(set(['34013']), 906.236730753, 206.977429459, 793633, 7.1e-05), Cluster(set(['34039']), 905.587082153, 210.045085725, 522541, 7.3e-05), Cluster(set(['34017']), 909.08042421, 207.462937763, 608975, 9.1e-05), Cluster(set(['36061']), 911.072622034, 205.783086757, 1537195, 0.00015), Cluster(set(['36005']), 912.315497328, 203.674106811, 1332650, 0.00011), Cluster(set(['36047']), 911.595580089, 208.928374072, 2465326, 9.8e-05), Cluster(set(['36059']), 917.384980291, 205.43647538, 1334544, 7.6e-05), Cluster(set(['36081']), 913.462051588, 207.615750359, 2229379, 8.9e-05), Cluster(set(['41051']), 103.293707198, 79.5194104381, 660486, 9.3e-05), Cluster(set(['41067']), 92.2254623376, 76.2593957841, 445342, 7.3e-05), Cluster(set(['51013']), 865.681962839, 261.222875114, 189453, 7.7e-05), Cluster(set(['51840']), 845.843602685, 258.214178983, 23585, 7.1e-05), Cluster(set(['51760']), 865.424050159, 293.735963553, 197790, 8.6e-05), Cluster(set(['55079']), 664.855000617, 192.484141264, 940164, 7.4e-05), Cluster(set(['54009']), 799.221537984, 240.153315109, 25447, 7.7e-05),Cluster(set(['11001']), 867.470401202, 260.460974222, 572059, 7.7e-05)], 15, 1)
+n=0
+for x in m:
+    for m in x:
+        n+=1
+        print n
